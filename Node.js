5e7859b535053e25f457dcd5f58de3f50830fca1
sonali kurade <sonali.k593@gmail.com>
2025-04-17 10:54:50 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,7 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Azure App Service (Node.js)!'));
+
+app.listen(process.env.PORT || 3000);
